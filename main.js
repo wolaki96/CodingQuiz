d540bea3b055466bd6593f22a2da9f56d6f11502
Squiddy <wolaki96@gmail.com>
2022-12-11 11:47:48 -0500

    added variables

---
+++ main.js
@@ -0,0 +1,16 @@
+const currentQuestion = 0;
+const timeLeft = 0;
+const score = 0;
+const timerEl = document.getElementById("timer");
+var timerInterval;
+var startButton = document.getElementById("startButton");
+
+/*start the quiz*/
+function startQuiz() {
+
+}
+
+/*start the timer*/
+function startTimer() {
+    
+}
